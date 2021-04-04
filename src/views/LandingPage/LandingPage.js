601944df4258649d0bc9c13cb54321c065e646a9
kiran kodali <kirankodali10@icloud.com>
2021-04-04 20:44:12 +0530

    master

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,9 +1,18 @@
 import { React } from "react";
+import { Row, Col } from "reactstrap";
+import '../../assets/css/landing-page.css';
+import background from '../../assets/img/landing-page-image-2.jpg'
 
-export default function LandingPage(){
-    return(
-        <div>
-            <h3>This is landing page</h3>
+export default function LandingPage() {
+    return (
+        <div className="profile-photo">
+            <div className="mask-background flex-center">
+                <div className="main-typography text-center align-center">
+                    <h1>KIRAN KODALI</h1>
+                    <h3>Software Developer</h3>
+                </div>
+            </div>
+
         </div>
     )
 }
